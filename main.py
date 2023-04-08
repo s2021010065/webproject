be8c747a51b7bea36bb7d9f4712ba14ae34c99ce
s2021010065 <118541652+s2021010065@users.noreply.github.com>
2023-04-08 20:13:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+#функция-обработчик сообщений
+async def echo(update, context):
+    pass
+
+
+#регистрация пользователя
+async def reg():
+    pass
+
+#симптомы
+async def sympt():
+    pass
+
+#болезни
+async def pains():
+    pass
+
+#меры профилактики, лекарства
+async def health():
+    pass
